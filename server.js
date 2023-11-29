e017c2e8cd97573fd11eb14d0f4e7ff10ad1502b
Jeff Spacek <Jeffrey.spacek@gmail.com>
2023-11-29 04:57:33 -0800

    fix
    updated code, added tracking

--- server.js
+++ server.js
@@ -1,52 +1,37 @@
 
-//Directory//
-
-const http = require('http');
-fs = require('fs'),
+  const http = require('http'),
+  fs = require('fs'),
   url = require('url');
 
-
-//HTTP Module//
 http.createServer((request, response) => {
   let addr = request.url,
-    q = new URL(addr, 'http://localhost:8080'),
+    q = new URL(addr, 'http://' + request.headers.host),
     filePath = '';
 
-    if (q.pathname.includes('documentation')) {
-      filePath = (__dirname + '/documentation.html');
+  fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', (err) => {
+    if (err) {
+      console.log(err);
     } else {
-      filePath = 'index.html';
+      console.log('Added to log.');
     }
-  
-    fs.readFile(filePath, (err, data) => {
-      if (err) {
-        throw err;
-      }
-  
-      response.writeHead(200, { 'Content-Type': 'text/html' });
-      response.write(data);
-      response.end();
-  
-    });
-  
-  }).listen(8080);
-  console.log('My test server is running on Port 8080.');
+  });
 
-
-//URL Module//
-let addr = request.url;
-let q = new URL(addr, 'http://localhost:8080');
-
-//FS System Module//
-const fs = require("fs");
-
-fs.readFile('input.txt', (err, data) => {
-  if (err) {
-    throw err;
+  if (q.pathname.includes('documentation')) {
+    filePath = (__dirname + '/documentation.html');
+  } else {
+    filePath = 'index.html';
   }
-  console.log('File content: ' + data.toString());
-});
 
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      throw err;
+    }
 
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write(data);
+    response.end();
 
+  });
 
+}).listen(8080);
+console.log('My test server is running on Port 8080.');
