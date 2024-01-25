efd46fafd1b89c5e67fa7a3bbed8ce9916dd18cd
Jeff Spacek <Jeffrey.spacek@gmail.com>
2024-01-25 06:14:28 -0800

    removed passport authenticate from endpoint /movies

--- index.js
+++ index.js
@@ -336,7 +336,6 @@ app.get(
 // generate a list of the entire movie collection
 app.get(
   "/movies",
-  passport.authenticate("jwt", { session: false }),
   async (req, res) => {
     await movies
       .find()
